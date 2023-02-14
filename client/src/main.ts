307d1f2a427405b5f0720d90f952e8c4d9e8fadf
Julian Pollinger <julian@pollinger.dev>
2023-02-14 14:48:46 +0100

    feat(client): styling

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,7 +4,8 @@ import {createPinia} from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootswatch/dist/cosmo/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import './assets/main.css';
 
