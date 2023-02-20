815acccf0ef68b2bcf13cddb1bade93b05623618
Julian Pollinger <julian@pollinger.dev>
2023-02-20 22:29:55 +0100

    feat(client): show libraries and their licenses on about page
    Signed-off-by: Julian Pollinger <julian@pollinger.dev>

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,10 +1,21 @@
 <template>
-<h1>TimeClicker v{{version}}</h1>
+  <h1>TimeClicker v{{ version }}</h1>
   <p>Entwickelt von Educorvi</p>
   <hr>
   <b-card header="Changelog">
     <span v-if="changelog.length" v-html="changelog">
     </span>
+    <custom-spinner v-else/>
+  </b-card>
+  <hr>
+  <h2>Verwendete Bibliotheken</h2>
+  <b-card class="mb-4" v-for="license of licenses" :header="`${license.name}@${license.installedVersion} by ${license.author}`">
+    <b>Source Code:</b> {{license.link}}
+    <hr>
+    <div v-if="licenseTexts.get(license.name)">
+      <b>License text:</b><br>
+      {{licenseTexts.get(license.name)}}
+    </div>
     <custom-spinner v-else/>
   </b-card>
 </template>
@@ -17,14 +28,34 @@ import {onMounted, ref} from "vue";
 import CustomSpinner from "@/components/CustomSpinner.vue"
 import {Converter} from "showdown"
 
-const converter = new Converter();
+type License = {
+  "name": string,
+  "licenseType": string,
+  "link": string,
+  "installedVersion": string,
+  "author": string
+}
 
 const changelog = ref("");
+const licenses = ref<License[]>([])
+const licenseTexts = ref(new Map<string, string>());
 
 onMounted(() => {
-  axios.get("/CHANGELOG.md").then(res => {
-    changelog.value = converter.makeHtml(res.data);
+  axios.get("/CHANGELOG.md").then(({data}) => {
+    const converter = new Converter();
+    changelog.value = converter.makeHtml(data);
   });
+  axios.get("/licenses.json").then(({data}) => {
+    for (const datum of data) {
+      licenses.value.push(datum);
+    }
+    licenses.value.forEach((l) => {
+      axios.get("/license-files/" + l.name + ".LICENSE.txt").then(({data}) => {
+        licenseTexts.value.set(l.name, data);
+      });
+    });
+  })
+
 });
 </script>
 
