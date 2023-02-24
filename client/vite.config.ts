096bdbcfecf53ff784905df44eb6f4a8d31a14ff
Julian Pollinger <julian@pollinger.dev>
2023-02-24 23:21:38 +0100

    chore: add vite-pwa-plugin
    Signed-off-by: Julian Pollinger <julian@pollinger.dev>

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,23 +1,32 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        compatConfig: {
-          MODE: 2
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    compatConfig: {
+                        MODE: 2
+                    }
+                }
+            }
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,txt,json}']
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            vue: "@vue/compat"
         }
-      }
     }
-  })],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      vue: "@vue/compat"
-    }
-  }
 })
