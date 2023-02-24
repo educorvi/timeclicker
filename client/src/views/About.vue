f1b586660e2a34ec8767ba16f3f5432d19b6138d
Julian Pollinger <julian@pollinger.dev>
2023-02-24 23:13:02 +0100

    fix: about page design
    Signed-off-by: Julian Pollinger <julian@pollinger.dev>

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,28 +1,35 @@
 <template>
-  <h1>TimeClicker v{{ version }}</h1>
-  <p>Entwickelt von Educorvi</p>
+  <div class="w-100 text-center">
+    <h1>TimeClicker v{{ version }}</h1>
+    <p>Entwickelt von Educorvi</p>
+  </div>
   <hr>
-  <b-card header="Changelog">
+  <b-button-group class="w-100">
+      <b-button variant="outline-light" v-b-modal.changelog-modal>Changelog</b-button>
+    <b-button variant="outline-light" v-b-modal.license-modal>Bibliotheken</b-button>
+  </b-button-group>
+  <b-modal id="changelog-modal" title="Changelog" size="xl" centered hide-footer scrollable>
     <span v-if="changelog.length" v-html="changelog">
     </span>
     <custom-spinner v-else/>
-  </b-card>
-  <hr>
-  <h2>Verwendete Bibliotheken</h2>
-  <b-card class="mb-4" v-for="license of licenses" :header="`${license.name}@${license.installedVersion} by ${license.author}`">
-    <b>Source Code:</b> {{license.link}}
-    <hr>
-    <div v-if="licenseTexts.get(license.name)">
-      <b>License text:</b><br>
-      {{licenseTexts.get(license.name)}}
-    </div>
-    <custom-spinner v-else/>
-  </b-card>
+  </b-modal>
+  <b-modal id="license-modal" title="Verwendete Bibliotheken" size="xl" centered hide-footer scrollable>
+    <b-card class="mb-4" v-for="license of licenses" :header="`${license.name}@${license.installedVersion} by ${license.author}`">
+      <b>Source Code:</b> {{license.link}}
+      <hr>
+      <div v-if="licenseTexts.get(license.name)">
+        <b>License text:</b><br>
+        {{licenseTexts.get(license.name)}}
+      </div>
+      <custom-spinner v-else/>
+    </b-card>
+  </b-modal>
+
 </template>
 
 <script setup lang="ts">
 import {version} from "../../package.json"
-import {BCard} from "bootstrap-vue";
+import {BCard, BButton, BButtonGroup, BModal} from "bootstrap-vue";
 import axios from "axios";
 import {onMounted, ref} from "vue";
 import CustomSpinner from "@/components/CustomSpinner.vue"
