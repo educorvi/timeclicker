c78156387b04b0116cf173f74da570d5fdec01f6
Julian Pollinger <julian@pollinger.dev>
2023-03-02 09:34:34 +0100

    chore: pwa settings
    Signed-off-by: Julian Pollinger <julian@pollinger.dev>

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -20,6 +20,37 @@ export default defineConfig({
             registerType: 'autoUpdate',
             workbox: {
                 globPatterns: ['**/*.{js,css,html,ico,png,svg,txt,json}']
+            },
+            devOptions: {enabled: true},
+            manifest: {
+                name: "Timeclicker",
+                short_name: "Clicker",
+                description: "An app to log your hours",
+                theme_color: "#375A7F",
+                icons: [
+                    {
+                        src: '/icons/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/android-chrome-maskable-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    },
+                    {
+                        src: '/icons/android-chrome-maskable-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    },
+                ]
             }
         })
     ],
