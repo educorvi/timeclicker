58c15c42e1bca4d11d25579c65cb80349da924c8
Julian Pollinger <julian@pollinger.dev>
2023-04-05 19:13:21 +0200

    fix: fix ui bug that broke ui
    Signed-off-by: Julian Pollinger <julian@pollinger.dev>

---
+++ client/src/i18n/index.ts
@@ -0,0 +1,41 @@
+import { createI18n } from "vue-i18n";
+import messages from "@/i18n/messages";
+
+let lang = localStorage.getItem("language");
+if (!lang) {
+  const navLang = navigator.language.split("-")[0];
+  if (Object.keys(messages).includes(navLang)) {
+    lang = navLang;
+  } else {
+    console.info(`Language ${navLang} is not supported. Using en instead.`)
+    lang = `en`;
+  }
+  localStorage.setItem("language", lang);
+}
+
+export default createI18n({
+  locale: lang,
+  legacy: false,
+  fallbackLocale: 'en',
+  messages,
+  datetimeFormats: {
+    'en': {
+      short: {
+        year: 'numeric', month: 'short', day: 'numeric',
+      },
+      long: {
+        year: 'numeric', month: 'short', day: 'numeric',
+        weekday: 'short', hour: 'numeric', minute: 'numeric'
+      }
+    },
+    'de': {
+      short: {
+        year: 'numeric', month: 'short', day: 'numeric'
+      },
+      long: {
+        year: 'numeric', month: 'short', day: 'numeric',
+        weekday: 'short', hour: 'numeric', minute: 'numeric'
+      }
+    },
+  }
+})
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,5 @@
 import {createApp} from 'vue';
 import {createPinia} from 'pinia';
-import {createI18n} from "vue-i18n";
 
 import App from './App.vue';
 import router from './router';
@@ -8,21 +7,11 @@ import router from './router';
 import './main.scss';
 
 import {ModalPlugin, FormPlugin, FormInputPlugin, FormSelectPlugin, ToastPlugin} from "bootstrap-vue";
-import messages from "@/i18n/messages";
+import i18n from "@/i18n";
 
 const app = createApp(App);
 
-let lang = localStorage.getItem("language");
-if (!lang) {
-    const navLang = navigator.language.split("-")[0];
-    if (Object.keys(messages).includes(navLang)) {
-        lang = navLang;
-    } else {
-        console.info(`Language ${navLang} is not supported. Using en instead.`)
-        lang = `en`;
-    }
-    localStorage.setItem("language", lang);
-}
+
 
 app.use(createPinia());
 app.use(router);
@@ -31,31 +20,6 @@ app.use(FormPlugin);
 app.use(FormInputPlugin);
 app.use(FormSelectPlugin);
 app.use(ToastPlugin);
-app.use(createI18n({
-    locale: lang,
-    legacy: false,
-    fallbackLocale: 'en',
-    messages,
-    datetimeFormats: {
-        'en': {
-            short: {
-                year: 'numeric', month: 'short', day: 'numeric',
-            },
-            long: {
-                year: 'numeric', month: 'short', day: 'numeric',
-                weekday: 'short', hour: 'numeric', minute: 'numeric'
-            }
-        },
-        'de': {
-            short: {
-                year: 'numeric', month: 'short', day: 'numeric'
-            },
-            long: {
-                year: 'numeric', month: 'short', day: 'numeric',
-                weekday: 'short', hour: 'numeric', minute: 'numeric'
-            }
-        },
-    }
-}))
+app.use(i18n)
 
 app.mount('#app');
