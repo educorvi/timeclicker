22eed5282db4fae2609de6fb28837edf1b331154
Julian Pollinger <julian@pollinger.dev>
2023-04-05 10:49:14 +0200

    docs: document classes

---
+++ server/src/testing_datasource.ts
@@ -0,0 +1,14 @@
+import User from "./classes/User";
+import Activity from "./classes/Activity";
+import Task from "./classes/Task";
+import {DataSource} from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "superdupertest",
+    database: "timeclicker",
+    entities: [User, Activity, Task],
+});
+
