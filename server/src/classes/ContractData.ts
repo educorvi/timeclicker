5e97d57f3abd501411a7fa4f277ea173001aa4c0
Julian Pollinger <julian@pollinger.dev>
2024-06-07 16:09:21 +0200

    contractData

---
+++ server/src/classes/ContractData.ts
@@ -0,0 +1,38 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import User from './User';
+
+/**
+ * Data about a user's contracts
+ */
+@Entity()
+export default class ContractData {
+    /**
+     * The ID
+     */
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    /**
+     * The user that this contract data belongs to
+     */
+    @ManyToOne(() => User, (user) => user.contractData, { nullable: false, onDelete: 'CASCADE' })
+    user: User;
+
+    /**
+     * Hours that the user is supposed to work per month
+     */
+    @Column()
+    hoursPerMonth: number;
+
+    /**
+     * The month in which the contract starts
+     */
+    @Column({type: 'int'})
+    startMonth: number;
+
+    /**
+     * The year in which the contract starts
+     */
+    @Column({type: 'int'})
+    startYear: number;
+}
