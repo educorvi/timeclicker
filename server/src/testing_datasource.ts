8425105594559dabc7a5f3e0d1e5b3cf287c8b24
Julian Pollinger <julian@pollinger.dev>
2024-06-14 11:07:16 +0200

    vacation schema

--- server/src/testing_datasource.ts
+++ server/src/testing_datasource.ts
@@ -1,9 +1,5 @@
-import User from './classes/User';
-import Activity from './classes/Activity';
-import Task from './classes/Task';
 import { DataSource } from 'typeorm';
-import ContractData from './classes/ContractData';
-import WorkingHours from './classes/WorkingHours';
+import { User, Activity, Task, ContractData, WorkingHours, Vacation } from './classes';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
@@ -11,5 +7,5 @@ export const AppDataSource = new DataSource({
     username: 'postgres',
     password: 'superdupertest',
     database: 'timeclicker',
-    entities: [User, Activity, Task, ContractData, WorkingHours],
+    entities: [User, Activity, Task, ContractData, WorkingHours, Vacation],
 });
