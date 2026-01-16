2693da2df56557974270de707eb6958c2e0b6d5f
Julian Pollinger <julian@pollinger.dev>
2026-01-16 16:37:43 +0100

    feat: remove working hours from server
    BREAKING CHANGE: Removed working hours

--- server/src/controllers/orgaController.ts
+++ server/src/controllers/orgaController.ts
@@ -6,24 +6,12 @@ import {
     Request,
     Get,
     Query,
-    Post,
-    Body,
-    Delete,
-    Path,
 } from 'tsoa';
 import type express from 'express';
 import { UnauthorizedError } from '../authentication';
 import { db } from '../globals';
 import { Any, LessThanOrEqual, MoreThanOrEqual } from 'typeorm';
-import { ContractData } from '../classes';
-import { calculateAllTimeBalances, calculateTimeBalance } from '../timeBalanceCalculation';
-import { getAllVacationDayData } from '../vacationDayCalculation';
-import type { VacationDayData } from '../vacationDayCalculation';
 
-export type saveContractDataParams = Omit<ContractData, 'id' | 'user'> & {
-    id?: string;
-    userId: string;
-};
 
 function checkOrgaStatus(req: express.Request): void {
     const tokStr = req.rawHeaders[req.rawHeaders.indexOf('token') + 1];
@@ -87,28 +75,6 @@ export class OrgaController extends Controller {
         });
     }
 
-    /**
-     * Create or update contractData. If `id` is passed in the body, the contractData with this id will be updated.
-     * @param requestBody
-     * @param req
-     */
-    @Post('contract-data')
-    @Response(404, 'User not found')
-    public async saveContractData(
-        @Body() requestBody: saveContractDataParams,
-        @Request() req: express.Request,
-    ) {
-        checkOrgaStatus(req);
-        const user = await db.getUser(requestBody.userId);
-        if (!user) {
-            this.setStatus(404);
-            return;
-        }
-        return db.saveContractData({
-            ...requestBody,
-            user,
-        });
-    }
 
     /**
      * Returns a list of all contracts associated with the given user
@@ -127,69 +93,5 @@ export class OrgaController extends Controller {
             this.setStatus(404);
             return;
         }
-        return db.getContractData({
-            where: { user },
-            order: { startYear: 'ASC', startMonth: 'ASC' },
-        });
-    }
-
-    /**
-     * Deletes the contract data with the given id
-     * @param req
-     * @param contractId The id of the contract data
-     */
-    @Delete('contract-data/{contractId}')
-    public async deleteContractData(
-        @Request() req: express.Request,
-        @Path() contractId: string,
-    ) {
-        checkOrgaStatus(req);
-        const contractData = await db.getContractDataById(contractId);
-        if (!contractData) {
-            this.setStatus(404);
-            return;
-        }
-        await db.deleteContractData(contractData);
-    }
-
-    /**
-     * Calculate the time balance for all users
-     * @param req
-     * @param from Specifies from which date the time balance should be included in the response. This does not affect the calculation.
-     * @param to Specifies until which date the time balance should be included in the response. This does not affect the calculation.
-     */
-    @Get('time-balance')
-    public async getTimeBalance(@Request() req: express.Request, @Query() from?: Date, @Query() to?: Date){
-        checkOrgaStatus(req);
-        return await calculateAllTimeBalances(from, to);
-    }
-
-
-    /**
-     * Calculates the time balance for the given user
-     * @param req
-     * @param userId The id of the user
-     * @param from Specifies from which date the time balance should be included in the response. This does not affect the calculation.
-     * @param to Specifies until which date the time balance should be included in the response. This does not affect the calculation.
-     */
-    @Get('time-balance/{userId}')
-    @Response(404, 'User not found')
-    public async getTimeBalanceForUser(@Request() req: express.Request, @Path() userId: string, @Query() from?: Date, @Query() to?: Date){
-        checkOrgaStatus(req);
-        const user = await db.getUser(userId);
-        if (!user) {
-            this.setStatus(404);
-            return;
-        }
-        return await calculateTimeBalance(user, from, to);
-    }
-
-    /**
-     * Get vacation day data for all users
-     */
-    @Get('vacation-days')
-    public async getVacationDays(@Request() req: express.Request): Promise<Record<string, VacationDayData>> {
-        checkOrgaStatus(req);
-        return await getAllVacationDayData();
     }
 }
--- server/src/libindex.ts
+++ server/src/libindex.ts
@@ -1,4 +1,2 @@
 export * from './classes';
 export * from './controllers';
-export type * from './vacationDayCalculation';
-export type * from './timeBalanceCalculation';
