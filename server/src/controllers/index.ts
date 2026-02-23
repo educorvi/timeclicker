faead818c6e936339287e0807cf51474c7913e42
Julian Pollinger <julian@pollinger.dev>
2026-02-23 13:34:25 +0100

    chore: export statisticsController

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -3,3 +3,4 @@ export * from './taskController';
 export * from './orgaController';
 export * from './pingController';
 export * from './authDataController';
+export * from './statisticsController';
